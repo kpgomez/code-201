cd47465b78117c92b62715169bc4c18cc8cdc87f
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-02-11 18:13:03 -0600

    fixed some syntax, renamed variables

--- js/app.js
+++ js/app.js
@@ -1,92 +1,82 @@
 'use strict';
 
 
-let visitorName = prompt("Please enter your name.");
+let visitorName = prompt('Please enter your name.');
 console.log(visitorName);
 
-let message = alert("Welcome " + visitorName + "!");
+let message = alert('Welcome ' + visitorName + "!");
+//let message = alert('Welcome ${visitorName}!'); template literal did not work
 console.log(message); //shows as "undefined" in console
 //let newMessage = document.write("Let's play a game!");
 //console.log(newMessage); //shows as "undefined" in console
 
-// question 1
-let start = confirm("Let's see how much you know about me, "+ visitorName + "." + "\nAre you ready for a short quiz?");
+
+let start = confirm('Are you ready for a short quiz?');
 console.log(start); // cancel === no, ok === yes
 
 if(start === true){
-    let begin = alert("OK, great to hear " + visitorName + ".");
+    let begin = alert('OK, great to hear ' + visitorName + '.');
     console.log(begin);
 } else {
-    let boo = alert("😢😪😭");
-}
+    let boo = alert('😢😪😭');
+};
 
-// question 1
 
-let answerToSchool = prompt("First Question: Did I graduate high school in the early 2000s? \nPlease answer 'yes' or 'no'.");
-console.log(answerToSchool);
+let school = prompt("Did I graduate high school in the early 2000s? \nPlease answer 'yes' or 'no'.").toLowerCase();
+console.log(school);
 
-let answerToSchoolLower = answerToSchool.toLowerCase();
-console.log(answerToSchoolLower);
-
-if(answerToSchoolLower === 'yes' || answerToSchoolLower === 'y'){
-    alert("You are correct! \nMy 20 year high school reunion is at the end of this year.");
-} else if(answerToSchoolLower === 'no' || answerToSchoolLower === 'n'){
-    alert("You are so kind but my 20 year high school reunion is at the end of this year. But watch out world because I still have so much more to offer!");
+if(school === 'yes' || school === 'y'){
+    alert('You are correct! \nMy 20 year high school reunion is at the end of this year.');
+} else if(school === 'no' || school === 'n'){
+    alert('You are so kind but my 20 year high school reunion is at the end of this year. But watch out world because I still have so much more to offer!');
 } else{
     alert("Please remember to answer as 'yes' or 'no'.");
 };
 
-let answerToFirstJob = prompt("Question #2: Was my first job in an office? \nPlease answer 'yes' or 'no'.");
-console.log(answerToFirstJob);
+let job = prompt('Was my first job in an office?').toLowerCase();
+console.log(job);
 
-let answerToFirstJobLower = answerToFirstJob.toLowerCase();
-console.log(answerToFirstJobLower);
 
-if(answerToFirstJobLower === 'yes' || answerToFirstJob === 'y'){
-    alert("I wish! \nMy first job was in a chicken joint when I was 15. \nI was the cashier/dishwasher/cook/floor mopper/window washer for 5.15 per hour. It was the longest 3 weeks of my life!");
-} else if(answerToFirstJobLower === 'no' || answerToFirstJobLower === 'n'){
-    alert("You are correct. My first job was in the fast-food industry.");
+if(job === 'yes' || job === 'y'){
+    alert('I wish! \nMy first job was in a chicken joint when I was 15. \nI was the cashier/dishwasher/cook/floor mopper/window washer for 5.15 per hour. It was the longest 3 weeks of my life!');
+} else if(job === 'no' || job === 'n'){
+    alert('You are correct. My first job was in the fast-food industry.');
 } else{
     alert("Please remember to answer as 'yes' or 'no'.");
 };
 
-let answerToSon = prompt("Question #3: Did I name my son after a video game? \nPlease answer 'yes' or 'no'.");
-console.log(answerToSon);
+let dress = prompt('Do I prefer comfort over style?').toLowerCase();
+console.log(dress);
 
-let answerToSonLower = answerToSon.toLowerCase();
-
-if(answerToSonLower === 'yes' || answerToSonLower === 'y'){
-    alert("I totally did! I sure hope my son does not resent me for that when he gets older.");
-} else if(answerToSonLower === 'no' || answerToSonLower === 'n'){
-    alert("You are correct. His middle name was inspired by a character from Street Fighter .");
-} else{
-    alert("Please remember to answer as 'yes' or 'no'.");
-};
-
-let answerToDress = prompt("Question #4: Do I prefer comfort over style? \nPlease answer 'yes' or 'no'.");
-console.log(answerToDress);
-
-let answerToDressLower = answerToDress.toLowerCase();
-
-if(answerToDressLower === 'yes' || answerToDressLower === 'y'){
-    alert("Woohoo to hoodies and sweats all day long!");
-} else if(answerToDressLower === 'no' || answerToDressLower === 'n') {
-    alert("Nope! I am completely okay with wearing the same thing everyday.");
+if(dress === 'yes' || dress === 'y'){
+    alert('Woohoo to hoodies and sweats all day long!');
+} else if(dress === 'no' || dress === 'n') {
+    alert('My personal style can be very hobo-ish if you know what I mean.');
 } else{
     alert("Please remember to answer as 'yes' or 'no'.")
 };
 
-let answerToShopping = prompt("Last Question: Do I love to shop? \nPlease answer 'yes' or 'no'.");
-console.log(answerToShopping);
+let son = prompt('Did I name my son after a video game?').toLowerCase();
+console.log(son);
 
-let answerToShoppingLower = answerToShopping.toLowerCase();
 
-if(answerToShoppingLower === 'yes' || answerToShoppingLower === 'y'){
-    alert("Yes! I love to BARGAIN shop at the thrift stores!! My favorite finds include a real jade bangle, a 1980s vintage designer gown, and a 2005 edition of the World of Warcraft board game.");
-} else if(answerToShoppingLower === 'no' || answerToShoppingLower === 'n') {
-    alert("You are partially right. I can do maybe an an hour in the mall, but I can spend all day in the thrift stores!");
+if(son === 'yes' || son === 'y'){
+    alert('I totally did! I sure he does not resent me for that when he gets older.');
+} else if(son === 'no' || son === 'n'){
+    alert('You are correct. His middle name was inspired by a character from Street Fighter .');
 } else{
     alert("Please remember to answer as 'yes' or 'no'.");
 };
 
-let finalMessage = alert("Thank you " + visitorName + " for visiting my site and completing the quiz! I hope you learned some fun things about me.");
+let thrift = prompt('Do I like spending all day in a thrift store?').toLowerCase();
+console.log(thrift);
+
+if(thrift === 'yes' || thrift === 'y'){
+    alert('Yes! I love to BARGAIN shop at the thrift stores!! My favorite finds include a real jade bangle, a 1980s vintage designer gown, and a 2005 edition of the World of Warcraft board game.');
+} else if(thrift === 'no' || thrift === 'n') {
+    alert('You are partially right. I can do maybe an an hour in the mall, but I can spend all day in the thrift stores!');
+} else{
+    alert("Please remember to answer as 'yes' or 'no'.");
+};
+
+let finalMessage = alert('Thank you ' + visitorName + ' for visiting my site and completing the quiz! I hope you learned some fun things about me.');
